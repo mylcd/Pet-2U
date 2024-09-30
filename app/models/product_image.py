1df24d4f98675fbba14142179eb80078e33ec5d4
mylcd <yinlongmi@gmail.com>
2024-09-30 12:52:01 -0700

    Create Model Table: ProductImages

---
+++ app/models/product_image.py
@@ -0,0 +1,17 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class ProductImage(db.Model):
+  __tablename__ = 'product_images'
+
+  if environment == 'production':
+    __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('products.id')), nullable=False)
+  url = db.Column(db.Integer, nullable=False)
+
+  created_on = db.Column(db.DateTime, default=datetime.now)
+  updated_on = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+  product = db.relationship('Product', back_populates="product_images")
