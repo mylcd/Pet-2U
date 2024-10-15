de463c7b9d44d7b6d2061f6bcfd70b0e83f68292
mylcd <yinlongmi@gmail.com>
2024-10-14 17:21:42 -0700

    Expand Product Image description field length

--- app/models/product_image.py
+++ app/models/product_image.py
@@ -10,7 +10,7 @@ class ProductImage(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('products.id')), nullable=False)
-  url = db.Column(db.String(1000), nullable=False)
+  url = db.Column(db.String(10000), nullable=False)
 
   created_on = db.Column(db.DateTime, default=datetime.now())
   updated_on = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
