9854b6893f9759273fded00f6c84c340a1d6a0ef
mylcd <yinlongmi@gmail.com>
2024-12-14 11:16:41 -0800

    Added Needed Data for Profile Page

--- react-vite/src/components/ProfilePage/ProfilePage.jsx
+++ react-vite/src/components/ProfilePage/ProfilePage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 //import { useNavigate } from "react-router-dom";
 import { getAllOrders } from "../../redux/order";
+import Default from "../ProductComponents/default.png";
 
 function ProfilePage() {
   const dispatch = useDispatch();
@@ -10,6 +11,7 @@ function ProfilePage() {
   const orders = useSelector(state => state.order.allOrders);
   console.log(orders);
   const sessionUser = useSelector(state => state.session.user);
+  console.log(sessionUser);
 
   useEffect(() => {
     dispatch(getAllOrders());
@@ -17,10 +19,22 @@ function ProfilePage() {
 
   return (
     <div>
+      <h1>{sessionUser.username}</h1>
+      <div>{sessionUser.email}</div>
+      <div>Orders:</div>
       {orders.map((order, i)=>{
         return (
-          <div key={order.id}>
-            {order.id}
+          <div key={i}>
+            {order.Products.map((product, j)=>{
+              console.log(product);
+              return (
+                <div key={"product" + j}>
+                  <img
+                    src={product.previewImg=="default.png"
+                    ? Default : product.previewImg}/>
+                </div>
+              )
+            })}
           </div>
         )
       })}
