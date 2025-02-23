83a6692c92b84dd1974dee35822875beadcade2a
mylcd <yinlongmi@gmail.com>
2025-02-22 16:47:48 -0800

    Order and Cart seeder documentation

--- app/seeds/cart_items.py
+++ app/seeds/cart_items.py
@@ -1,12 +1,20 @@
 from app.models import db, CartItem, environment, SCHEMA
 from sqlalchemy.sql import text
 
+# Adds 1 seeder shopping cart belongs to user_id=2
+# This shopping cart has 1 product with id=1
 def seed_carts():
   buying = CartItem(user_id=2, product_id=1, amount=1)
 
   db.session.add(buying)
   db.session.commit()
 
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
 def undo_carts():
   if environment == "production":
     db.session.execute(f"TRUNCATE table {SCHEMA}.cart_items RESTART IDENTITY CASCADE;")
--- app/seeds/orders.py
+++ app/seeds/orders.py
@@ -1,6 +1,9 @@
 from app.models import db, Order, OrderProduct, environment, SCHEMA
 from sqlalchemy.sql import text
 
+# Adds 1 seeder order with 2 different items included in this order
+# The order has 1 id=2 product @ $1000, 2 id=3 product @ $149
+# Price in order does not have to match price of product
 def seed_orders():
   order1 = Order(user_id=2)
 
@@ -13,6 +16,12 @@ def seed_orders():
 
   db.session.commit()
 
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
 def undo_orders():
   if environment == "production":
     db.session.execute(f"TRUNCATE table {SCHEMA}.order_products RESTART IDENTITY CASCADE;")
