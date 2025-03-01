ddfa918277c18cf2b4034850e36ee81ef76d662f
mylcd <yinlongmi@gmail.com>
2025-03-01 12:16:29 -0800

    Backend Forms fix format

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -5,24 +5,24 @@ from app.models import User
 
 
 def user_exists(form, field):
-    # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('Email provided not found.')
+  # Checking if user exists
+  email = field.data
+  user = User.query.filter(User.email == email).first()
+  if not user:
+    raise ValidationError('Email provided not found.')
 
 
 def password_matches(form, field):
-    # Checking if password matches
-    password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('No such user exists.')
-    if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+  # Checking if password matches
+  password = field.data
+  email = form.data['email']
+  user = User.query.filter(User.email == email).first()
+  if not user:
+    raise ValidationError('No such user exists.')
+  if not user.check_password(password):
+    raise ValidationError('Password was incorrect.')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired(), password_matches])
+  email = StringField('email', validators=[DataRequired(), user_exists])
+  password = StringField('password', validators=[DataRequired(), password_matches])
